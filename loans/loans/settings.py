5d6874e0183f94047710e8836d7be6dd8eafad3f
studen <jecintawanjirumuhia@gmail.com>
2025-06-25 07:31:20 +0300

    created an api  for document and farmerLoan app

--- loans/loans/settings.py
+++ loans/loans/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qj3_40%4=9h!6h8tt+1dr)-l0_fh120p^7hb4rh%4ns0g=r@14'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,11 +42,11 @@ INSTALLED_APPS = [
     'document',
     'cooperative',
     
-    
+      
      'farmer',
      'farmerWealth',
      'farmerLoan',
-     
+     'rest_framework',
     
 
 ]
