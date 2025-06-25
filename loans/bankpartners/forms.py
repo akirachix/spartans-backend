2f0a87807cb5575c2f2cefc3343d9688ba10b8f7
Emeline Niyogisubizo <rubayizaniyogisubizoemeline@gmail.com>
2025-06-25 15:40:24 +0300

    ignore files

--- loans/bankpartners/forms.py
+++ loans/bankpartners/forms.py
@@ -1,7 +1,1 @@
 from django import forms
-# from .models import CooperativePartnerBank
-
-# class CooperativePartnerBankForm(forms.ModelForm):
-#     class Meta:
-#         model = CooperativePartnerBank
-#         fields = '__all__'
