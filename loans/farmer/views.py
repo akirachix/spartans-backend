e2ffcc72c559d7f377ac5c79980f1b06c4b31d5c
Bonnette <bonnetteumu@gmail.com>
2025-06-25 14:42:33 +0300

    Added serializers, views,tests and urls on farmer and farmerWealth apps

---
+++ loans/farmer/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Farmer
+from .serializers import FarmerSerializer
+class FarmerViewSet(viewsets.ModelViewSet):
+   queryset = Farmer.objects.all()
+   serializer_class = FarmerSerializer
