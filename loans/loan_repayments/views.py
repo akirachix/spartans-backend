8df47a4f3edddd00be2289c36a68a5e78b0b5a88
Rabecah <rabeccahneemah@gmail.com>
2025-06-25 14:18:11 +0300

    made changes

--- loans/loan_repayments/views.py
+++ loans/loan_repayments/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+
+from .models import LoanRepayment
+
+from .serializers import LoanRepaymentSerializer
+
 # Create your views here.
+
+class LoanRepaymentViewSet (viewsets.ModelViewSet):
+    queryset = LoanRepayment.objects.all()
+    serializer_class = LoanRepaymentSerializer
--- loans/loans/settings.py
+++ loans/loans/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'loan_repayments',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
