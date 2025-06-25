1bbcf7bd7cdabf737f45a378ea5c48cd1eee3df4
Bonnette <bonnetteumu@gmail.com>
2025-06-25 08:11:03 +0300

    added new changes

--- loans/farmerWealth/views.py
+++ loans/farmerWealth/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import FarmerWealth
+from .serializers import FarmerWealthSerializer
+class FarmerWealthViewSet(viewsets.ModelViewSet):
+   queryset = FarmerWealth.objects.all()
+   serializer_class= FarmerWealthSerializer
 
-# Create your views here.
+
