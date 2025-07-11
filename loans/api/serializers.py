4147e9b9a68117bb7568957c6b2372b0f9b5a701
Rabecah <rabeccahneemah@gmail.com>
2025-07-11 16:06:40 +0300

    made required changes

--- loans/api/serializers.py
+++ loans/api/serializers.py
@@ -1,10 +1,8 @@
 from rest_framework import serializers
-
-from loan_repayments.models import   LoanRepayment
+from loan_repayments.models import LoanRepayment
 
 
 class LoanRepaymentSerializer(serializers.ModelSerializer):
-  
     class Meta:
         model = LoanRepayment
         fields = '__all__'
