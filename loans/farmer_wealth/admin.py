cf7d76f9287bd7c132aed7254b3ea9e1b4d29df3
Bonnette <bonnetteumu@gmail.com>
2025-07-11 15:00:30 +0300

    added new changes

--- loans/farmer_wealth/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-
-from .models import FarmerWealth
-admin.site.register(FarmerWealth)
--- loans/loans/settings.py
+++ loans/loans/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bankpartners',
-    'loan_repayments',
     'farmer_wealth',
     'rest_framework',
 ]
