c7dbc248d983501a4d02bd01c9f6cc612cc3cd94
khaenzelidorothy <khaenzelidorothy@gmail.com>
2025-07-23 14:37:34 +0300

    changes

--- api/serializers.py
+++ api/serializers.py
@@ -61,3 +61,45 @@ class DarajaAPISerializer(serializers.Serializer):
   class Meta:
       model = LoanRepayment
       fields = "__all__"
+
+
+
+class STKPushView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = STKPushSerializer(data=request.data)
+        if serializer.is_valid():
+            data = serializer.validated_data
+            daraja = DarajaAPI()
+            response = daraja.stk_push(
+                phone_number=data['phone_number'],
+                amount=data['amount'],
+                account_reference=data['account_reference'],
+                transaction_desc=data['transaction_desc']
+            )
+
+            checkout_request_id = response.get('CheckoutRequestID', None)
+
+            user = None
+            if request.user.is_authenticated:
+                user = AppUser.objects.get(user=request.user)
+
+            if checkout_request_id:
+
+                payment = PaymentDetails.objects.create(
+                    phone_number=data['phone_number'],
+                    amount=data['amount'],
+                    account_reference=data['account_reference'],
+                    transaction_desc=data['transaction_desc'],
+                    mpesa_checkout_id=checkout_request_id,
+                    quantity= 1,
+                    type = 'payment',
+                    condition='New',
+                    price=data['amount'],
+                )
+
+            return Response(response)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
